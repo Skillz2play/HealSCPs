5b42ecee1806f689cd944f81da17b015337fbd5b
Liassid <liassidarthur@gmail.com>
2021-08-06 13:01:20 +0500

    Removed useless usings, changed the way command work

--- HealSCPs/Plugin.cs
+++ HealSCPs/Plugin.cs
@@ -1,23 +1,17 @@
-﻿using Exiled.API.Enums;
-using Exiled.API.Features;
+﻿using Exiled.API.Features;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HealSCPs
 {
     public class Plugin : Plugin<Config>
     {
-
         private static readonly Plugin Singleton = new Plugin();
         public static Plugin Instance => Singleton;
 
         public override string Name => "HealSCPs";
         public override string Author => "Skillz2play";
         public override Version Version => new Version(1, 1, 0);
-        public override Version RequiredExiledVersion => new Version(2, 11, 0);
+        public override Version RequiredExiledVersion => new Version(2, 11, 1);
 
         private Plugin()
         {
