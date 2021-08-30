a2eac984c79af9d896b2c0265004853e7929349e
Skillz <jster723@gmail.com>
2021-08-30 17:26:32 -0400

    fixed config for pint's sanity

--- HealSCPs/Plugin.cs
+++ HealSCPs/Plugin.cs
@@ -5,31 +5,31 @@ namespace HealSCPs
 {
     public class Plugin : Plugin<Config>
     {
-		public override void OnEnabled()
+	public override void OnEnabled()
+	{
+            Plugin.singleton = this;
+            base.OnEnabled();
+	}
+
+	public override void OnDisabled()
+	{
+            Plugin.singleton = null;
+            base.OnDisabled();
+	}
+
+	public static Plugin Instance
+	{
+		get
 		{
-			Plugin.singleton = this;
-			base.OnEnabled();
+                return Plugin.singleton;
 		}
+	}
 
-		public override void OnDisabled()
-		{
-			Plugin.singleton = null;
-			base.OnDisabled();
-		}
+	public override string Name => "HealSCPs";
+	public override string Author => "Skillz2play";
+	public override Version Version => new Version(1, 4, 1);
+	public override Version RequiredExiledVersion => new Version(3, 0, 0);
 
-		public static Plugin Instance
-		{
-			get
-			{
-				return Plugin.singleton;
-			}
-		}
-
-		public override string Name => "HealSCPs";
-		public override string Author => "Skillz2play";
-		public override Version Version => new Version(1, 3, 0);
-		public override Version RequiredExiledVersion => new Version(3, 0, 0);
-
-		private static Plugin singleton;
+	private static Plugin singleton;
 	}
 }
