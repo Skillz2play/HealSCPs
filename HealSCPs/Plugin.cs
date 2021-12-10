218d419af1da81d9b46ebc7407433601b3140a13
Skillz <jster723@gmail.com>
2021-12-10 18:53:47 -0500

    Changed version number

--- HealSCPs/Plugin.cs
+++ HealSCPs/Plugin.cs
@@ -24,7 +24,7 @@ namespace HealSCPs
 	}
 	public override string Name => "HealSCPs";
 	public override string Author => "Skillz2play";
-	public override Version Version => new Version(1, 4, 2);
+	public override Version Version => new Version(1, 4, 3);
 	public override Version RequiredExiledVersion => new Version(4, 0, 0);
 
 	private static Plugin singleton;
