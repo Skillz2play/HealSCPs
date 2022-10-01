7eb45412a322313fefa6e7ce282c2eb92f726d15
Skillz <jster723@gmail.com>
2022-09-30 22:51:27 -0400

    zereth didn't like my indenting so he made me fix it

--- HealSCPs/Plugin.cs
+++ HealSCPs/Plugin.cs
@@ -5,28 +5,23 @@ namespace HealSCPs
 {
     public class Plugin : Plugin<Config>
     {
-	public override void OnEnabled()
-	{
-            Plugin.singleton = this;
-            base.OnEnabled();
-	}
-	public override void OnDisabled()
-	{
-            Plugin.singleton = null;
-            base.OnDisabled();
-	}
-	public static Plugin Instance
-	{
-            get
-            {
-                return Plugin.singleton;
-            }
-	}
-	public override string Name => "HealSCPs";
-	public override string Author => "Skillz2play";
-	public override Version Version => new Version(1, 6, 0);
-	public override Version RequiredExiledVersion => new Version(5, 0, 0);
+		public override void OnEnabled()
+		{
+				Plugin.singleton = this;
+				base.OnEnabled();
+		}
+		public override void OnDisabled()
+		{
+				Plugin.singleton = null;
+				base.OnDisabled();
+		}
+		public static Plugin Instance => singleton;
 
-	private static Plugin singleton;
+		public override string Name => "HealSCPs";
+		public override string Author => "Skillz2play";
+		public override Version Version => new Version(1, 6, 0);
+		public override Version RequiredExiledVersion => new Version(5, 0, 0);
+
+		private static Plugin singleton;
 	}
 }
